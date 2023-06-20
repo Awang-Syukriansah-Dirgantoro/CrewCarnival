683ff649520eeb1fb4e65d5fd1f3169813a8233c
Awang-Syukriansah-Dirgantoro <awangsyukriansahdirgantoro@gmail.com>
2023-06-20 10:06:16 +0700

    Initial Commit

---
+++ CrewCarnival/CrewCarnivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewCarnivalApp.swift
+//  CrewCarnival
+//
+//  Created by Awang Syukriansah Dirgantoro on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CrewCarnivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
