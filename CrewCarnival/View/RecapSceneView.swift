a38014f7b1fe815759e0a7b1d8d9b1d7faa18273
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2023-06-25 02:34:42 +0700

    recap

--- CrewCarnival/View/RecapSceneView.swift
+++ CrewCarnival/View/RecapSceneView.swift
@@ -8,26 +8,37 @@
 import SwiftUI
 
 struct RecapSceneView: View {
-    @State var lives = 0
+    @State var lives = 2
+    @State var isAnimate = false
     var body: some View {
         ZStack{
             if lives > 0 {
                 Image("scenewin")
                 HStack{
                     if lives == 1{
-                        Image("starfill")
-                        Image("star").resizable().frame(width: 80, height: 80).offset(y: -10)
-                        Image("star")
+                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     } else if lives == 2{
-                        Image("starfill")
-                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10)
-                        Image("star")
+                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     } else{
-                        Image("starfill")
-                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10)
-                        Image("starfill")
+                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     }
-                }.offset(y: -50)
+                }.offset(y: -50).onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        withAnimation {
+                            isAnimate = true
+                        }
+                    }
+                }
+                VStack(spacing: 0){
+                    Image("replay").resizable().frame(width: 150, height: 80)
+                    Image("home").resizable().frame(width: 150, height: 80)
+                }.offset(y: 115)
             } else{
                 Image("scenelose")
                 HStack{
