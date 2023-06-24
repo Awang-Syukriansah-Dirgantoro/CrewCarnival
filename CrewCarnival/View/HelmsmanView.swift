f7b8240a5ea75561a214a66441dcef1fe5c9f1ab
AiffahKiysa <aiffahwaafi6@gmail.com>
2023-06-24 21:27:44 +0700

    stearing wheel

--- CrewCarnival/View/HelmsmanView.swift
+++ CrewCarnival/View/HelmsmanView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct HelmsmanView: View {
     @State private var downloadAmount = 80.0
+    @State private var progressInstruction = 0.0
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
     @State private var gradient = LinearGradient(
         gradient: Gradient(colors: [Color(red: 0, green: 0.82, blue: 0.23)]),
         startPoint: .topLeading,
         endPoint: .bottomTrailing
     )
+    @State private var progress: CGFloat = 0
+    @State private var angle: CGFloat = 0
+    @State private var lastAngle: CGFloat = 0
+    @State private var length : CGFloat = 400
+    
     var body: some View {
         let gradientStyle = GradientProgressStyle(
             stroke: .clear,
@@ -85,16 +93,52 @@ struct HelmsmanView: View {
                             .font(Font.custom("Gasoek One", size: 20))
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color(red: 0.95, green: 0.74, blue: 0))
-                        Rectangle()
-                            .frame(height: 5)
-                            .offset(y: 40)
-                            .foregroundColor(Color(red: 0, green: 0.82, blue: 0.23))
+                        ProgressView("", value: progressInstruction, total: 100)
+                            .offset(y: 30)
+                            .onReceive(timer) { _ in
+                                if progressInstruction < 100 {
+                                    progressInstruction += 0.5
+                                }
+                            }
+                            .progressViewStyle(LinearProgressViewStyle(tint: Color(red: 0, green: 0.82, blue: 0.23)))
                     }
+                    
                     Image("StearingWheel")
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
+                        .scaledToFill()
                         .frame(width: 300, height: 300)
-                        .padding(.top, 150)
+                        .rotationEffect(
+                            .degrees(Double(self.angle)))
+                        .gesture(DragGesture()
+                            .onChanged{ v in
+                                let theta = (atan2(v.location.x - self.length / 2, self.length / 2 - v.location.y) - atan2(v.startLocation.x - self.length / 2, self.length / 2 - v.startLocation.y)) * 180 / .pi
+                                self.angle = theta + self.lastAngle
+                                print(self.angle)
+                                
+                                if (self.angle > 300){
+                                    self.angle = 300
+                                    self.progress = self.angle
+                                } else if (self.angle < 0){
+                                    if (self.angle < -300){
+                                        self.angle = -300
+                                        self.progress = 300
+                                    } else {
+                                        self.progress = self.angle * (-1)
+                                    }
+                                }
+                                else {
+                                    self.progress = self.angle
+                                }
+                                print(self.angle)
+                            }
+                            .onEnded { v in
+                                self.lastAngle = self.angle
+                            }
+                        )
+                        .offset(y: 150)
+        
+                    Spacer()
+                        .frame(height: 180)
                     VStack{
                         Text("Turn Progress")
                             .font(Font.custom("Krub-Regular", size: 14))
@@ -107,8 +151,8 @@ struct HelmsmanView: View {
                                 .frame(width: 125, height: 32)
                                 .clipped())
                             .cornerRadius(15)
-                            .padding(.bottom, -20)
-                        ProgressBar(progress: 100)
+                            .padding(.bottom, -15)
+                        ProgressBar(progress: self.progress)
                     }
                 }
             }.background(Image("BgHelmsman").resizable().scaledToFit())
