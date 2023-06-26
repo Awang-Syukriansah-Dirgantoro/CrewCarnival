b06d5374ff1d4438349d7c547820a4b933302775
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2023-06-26 09:06:14 +0700

    recap fix

--- CrewCarnival/View/RecapSceneView.swift
+++ CrewCarnival/View/RecapSceneView.swift
@@ -16,17 +16,17 @@ struct RecapSceneView: View {
                 Image("scenewin")
                 HStack{
                     if lives == 1{
-                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("star").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("star").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     } else if lives == 2{
-                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("star").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("star").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     } else{
-                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                        Image("starfill").scaleEffect(isAnimate ? 1.2 : 1.0).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                        Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     }
                 }.offset(y: -50).onAppear {
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
@@ -35,10 +35,10 @@ struct RecapSceneView: View {
                         }
                     }
                 }
-                VStack(spacing: 0){
-                    Image("replay").resizable().frame(width: 150, height: 80)
-                    Image("home").resizable().frame(width: 150, height: 80)
-                }.offset(y: 115)
+                HStack(spacing: 10){
+                    Image("okrecap").resizable().frame(width: 100, height: 40)
+                    Image("replayrecap").resizable().frame(width: 40, height: 40)
+                }.offset(y: 165)
             } else{
                 Image("scenelose")
                 HStack{
@@ -46,6 +46,10 @@ struct RecapSceneView: View {
                     Image("star").resizable().frame(width: 80, height: 80).offset(y: -10)
                     Image("star")
                 }.offset(y: -50)
+                HStack(spacing: 10){
+                    Image("okrecap").resizable().frame(width: 100, height: 40)
+                    Image("replayrecap").resizable().frame(width: 40, height: 40)
+                }.offset(y: 165)
             }
             
         }
