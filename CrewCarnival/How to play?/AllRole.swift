3d33d9288fab68d0ac7c7ece1ffcdb0778ae927f
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2023-06-27 08:44:48 +0700

    fix eror

--- CrewCarnival/How to play?/AllRole.swift
+++ CrewCarnival/How to play?/AllRole.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct AllRole: View {
     @State var currentTab = "role1"
     @State var roles = [
-        Role(id: 0, image: "role1", description: "Lookout has the duty to monitor the surrounding conditions and provide information to the responsible authorities"),
-        Role(id: 1, image: "role2", description: "Helmsman's duty is to drive the ship and pass through obstacles notified by the lookout"),
-        Role(id: 2, image: "role3", description: "The sailing master's duty is to adjust the sails which affect the speed of the ship"),
-        Role(id: 3, image: "role4", description: "Cabin boy is in charge of providing operational support and services on board"),
-        Role(id: 4, image: "role5", description: "Blacksmith is in charge of making or repairing the necessary items"),
+        Roles(id: 0, image: "role1", description: "Lookout has the duty to monitor the surrounding conditions and provide information to the responsible authorities"),
+        Roles(id: 1, image: "role2", description: "Helmsman's duty is to drive the ship and pass through obstacles notified by the lookout"),
+        Roles(id: 2, image: "role3", description: "The sailing master's duty is to adjust the sails which affect the speed of the ship"),
+        Roles(id: 3, image: "role4", description: "Cabin boy is in charge of providing operational support and services on board"),
+        Roles(id: 4, image: "role5", description: "Blacksmith is in charge of making or repairing the necessary items"),
     ]
     var body: some View {
         ZStack{
@@ -34,7 +34,7 @@ struct AllRole: View {
     
 }
 
-struct Role : Identifiable {
+struct Roles : Identifiable {
     var id: Int
     var image : String
     var description : String
