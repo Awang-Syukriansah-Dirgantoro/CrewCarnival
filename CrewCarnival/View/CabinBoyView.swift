998888ffef8bb471306afa05573e994ec1a122ed
Awang-Syukriansah-Dirgantoro <awangsyukriansahdirgantoro@gmail.com>
2023-06-27 08:52:12 +0700

    New Cabin Boy

---
+++ CrewCarnival/View/CabinBoyView.swift
@@ -0,0 +1,20 @@
+//
+//  CabinBoyView.swift
+//  CrewCarnival
+//
+//  Created by Awang Syukriansah Dirgantoro on 27/06/23.
+//
+
+import SwiftUI
+
+struct CabinBoyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CabinBoyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CabinBoyView()
+    }
+}
