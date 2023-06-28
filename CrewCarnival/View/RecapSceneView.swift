17451129b238f9b43cf0f928786f6b34515a3c1d
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2023-06-28 10:18:22 +0700

    fix recap

--- CrewCarnival/View/RecapSceneView.swift
+++ CrewCarnival/View/RecapSceneView.swift
@@ -14,9 +14,9 @@ struct RecapSceneView: View {
     var body: some View {
         ZStack{
             if show {
-                Color.black.opacity(show ? 0.5 : 0).animation(.easeInOut(duration: 1.0)).edgesIgnoringSafeArea(.all)
+                Color.black.opacity(show ? 0.5 : 0).animation(.easeInOut(duration: 1.0))
                 if lives > 0 {
-                    Image("scenewin").opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
+                    Image("scenewin").resizable().frame(width: 310,height: 480).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     HStack{
                         if lives == 1{
                             Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
@@ -31,22 +31,22 @@ struct RecapSceneView: View {
                             Image("starfill").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                             Image("starfill").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                         }
-                    }.offset(y: -50)
+                    }.offset(y: -45)
                 } else{
-                    Image("scenelose")
+                    Image("scenelose").resizable().frame(width: 310,height: 480).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     HStack{
                         Image("star").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                         Image("star").resizable().frame(width: 80, height: 80).offset(y: -10).scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                         Image("star").scaleEffect(isAnimate ? 1.0 : 0.7).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                    }.offset(y: -50)
+                    }.offset(y: -45)
                 }
                 HStack(spacing: 10){
                     Image("okrecap").resizable().frame(width: 100, height: 40).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
                     Image("replayrecap").resizable().frame(width: 40, height: 40).opacity(isAnimate ? 1.0 : 0.0).animation(.easeInOut(duration: 1.0))
-                }.offset(y: 165)
+                }.offset(y: 155)
             }
             
-        }.onAppear {
+        }.edgesIgnoringSafeArea(.all).onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                 withAnimation {
                     isAnimate = true
