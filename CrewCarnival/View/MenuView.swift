f26f560d4c05a768fbbc42f58b53fe94110ba5a4
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2023-06-30 10:43:42 +0700

    adding sounds

--- CrewCarnival/View/MenuView.swift
+++ CrewCarnival/View/MenuView.swift
@@ -6,50 +6,57 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct MenuView: View {
+    @ObservedObject var vm = AudioViewModel()
     @State var menu = -1
     
     var body: some View {
-        if menu == 0 {
-            PartyView(menu: $menu)
-        } else if menu == 1 {
-            
-        } else {
-            VStack {
-                Spacer()
-                Button {
-                    menu = 0
-                } label: {
-                    Text("Play")
-                        .foregroundColor(.yellow)
-                        .fontWeight(.bold)
-                        .frame(
-                            minWidth: 0,
-                            maxWidth: .infinity
-                        )
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 15)
-                            .fill(Color.black))
-                        .padding(.horizontal)
+        VStack {
+            if menu == 0 {
+                PartyView(menu: $menu)
+            } else if menu == 1 {
+                
+            } else {
+                VStack {
+                    Spacer()
+                    Button {
+                        menu = 0
+                    } label: {
+                        Text("Play")
+                            .foregroundColor(.yellow)
+                            .fontWeight(.bold)
+                            .frame(
+                                minWidth: 0,
+                                maxWidth: .infinity
+                            )
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.black))
+                            .padding(.horizontal)
+                    }
+                    Button {
+                        menu = 1
+                    } label: {
+                        Text("Tutorial")
+                            .foregroundColor(.yellow)
+                            .fontWeight(.bold)
+                            .frame(
+                                minWidth: 0,
+                                maxWidth: .infinity
+                            )
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.black))
+                            .padding(.horizontal)
+                    }
+                    Spacer()
                 }
-                Button {
-                    menu = 1
-                } label: {
-                    Text("Tutorial")
-                        .foregroundColor(.yellow)
-                        .fontWeight(.bold)
-                        .frame(
-                            minWidth: 0,
-                            maxWidth: .infinity
-                        )
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 15)
-                            .fill(Color.black))
-                        .padding(.horizontal)
-                }
-                Spacer()
             }
+        }
+        .onAppear{
+            vm.playSound(url: "menu")
         }
     }
 }
